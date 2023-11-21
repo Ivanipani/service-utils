68af2f96b50a119f71d259f9f53549cc903ff754
Ivan Perdomo <iperdomo2016@gmail.com>
2023-11-21 11:41:54 -0500

    config - getbool, getint, add tests

--- src/config.py
+++ src/config.py
@@ -2,23 +2,50 @@ import os
 
 from dotenv import load_dotenv
 
+# Parse .env file, load all environment variables
 load_dotenv()
 
 
 class ServiceConfigInterface:
     def __init__(self):
-        self.validate()  # raise exception, cannot instantiate
+        self.validate()  # raises exception, cannot instantiate
 
-    def get(self, key: str):
+    def getStr(self, key: str):
+        raise NotImplementedError
+
+    def getBool(self, key: str):
+        raise NotImplementedError
+
+    def getInt(self, key: str):
         raise NotImplementedError
 
     def validate(self):
         raise NotImplementedError
 
 
-class EnvironmentConfigInterface(ServiceConfigInterface):
+class EnvironmentConfig(ServiceConfigInterface):
     def __init__(self) -> None:
         super().__init__()
 
-    def get(self, key: str) -> str | None:
+    def getStr(self, key: str) -> str | None:
         return os.environ.get(key)
+
+    def getBool(self, key: str) -> bool | None:
+        value = os.environ.get(key)
+
+        if isinstance(value, str):
+            if value.lower() in ["true", "yes", "y"]:
+                return True
+            elif value.lower() in ["false", "no", "n"]:
+                return False
+        return None
+
+    def getInt(self, key: str) -> int | None:
+        value = os.environ.get(key)
+
+        if isinstance(value, str):
+            return int(value)
+        return None
+
+    def validate(self):
+        pass
---
+++ tests/test_config.py
@@ -0,0 +1,62 @@
+import pytest
+import os
+import logging
+
+import src.config as config
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def clean_env_bool_var():
+    os.environ.pop("TEST_GET_BOOL", None)
+    yield
+    os.environ.pop("TEST_GET_BOOL", None)
+
+
+@pytest.fixture
+def clean_env_int_var():
+    os.environ.pop("TEST_GET_INT", None)
+    yield
+    os.environ.pop("TEST_GET_INT", None)
+
+
+@pytest.mark.parametrize("env_value", ["True", "true", "truE", "YES", "yes", "y"])
+@pytest.mark.usefixtures("clean_env_bool_var")
+def test_get_bool_true(env_value):
+    os.environ["TEST_GET_BOOL"] = env_value
+    cfg = config.EnvironmentConfig()
+
+    assert cfg.getBool("TEST_GET_BOOL") == True
+
+
+@pytest.mark.parametrize("env_value", ["FALSE", "false", "NO", "no", "n"])
+@pytest.mark.usefixtures("clean_env_bool_var")
+def test_get_bool_false(env_value):
+    os.environ["TEST_GET_BOOL"] = env_value
+    cfg = config.EnvironmentConfig()
+
+    assert cfg.getBool("TEST_GET_BOOL") == False
+
+
+@pytest.mark.usefixtures("clean_env_bool_var")
+def test_get_bool_not_present():
+    cfg = config.EnvironmentConfig()
+    assert cfg.getBool("TEST_GET_BOOL") is None
+
+
+@pytest.mark.usefixtures("clean_env_int_var")
+def test_get_int():
+    os.environ["TEST_GET_BOOL"] = "4000"
+
+    cfg = config.EnvironmentConfig()
+    assert cfg.getInt("TEST_GET_BOOL") == 4000
+
+
+@pytest.mark.usefixtures("clean_env_int_var")
+def test_get_int_bad_value_raises_exception():
+    os.environ["TEST_GET_BOOL"] = "NOT INT"
+
+    cfg = config.EnvironmentConfig()
+    with pytest.raises(ValueError):
+        cfg.getInt("TEST_GET_BOOL")
